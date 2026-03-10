f42cdca7c0a0f5d04ad8ce4fb1d16160335f26a9
Mijan <skmijanhossain@gmail.com>
2026-03-10 12:21:02 +0530

    add

--- i18n.ts
+++ i18n.ts
@@ -8,14 +8,14 @@ import ar from './locales/ara.json';
 import hi from './locales/hin.json';
 import kn from './locales/kan.json';
 import ta from './locales/tam.json';
-import fa from './locales/fr.json';
+import fr from './locales/fr.json';
 
 import {iso6393To1} from 'iso-639-3';
 
 import {getItem} from './machines/store';
 import {LocalizedField} from './machines/VerifiableCredential/VCMetaMachine/vc';
 
-const resources = {en, fil, ar, hi, kn, ta};
+const resources = {en, fil, ar, hi, kn, ta,fr};
 const locale = Localization.locale;
 const languageCodeMap = {} as {[key: string]: string};
 
